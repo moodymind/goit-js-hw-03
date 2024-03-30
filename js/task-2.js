ed8d422c096a92e9c764712f48ad341e03d63674
Austin Miller <vadim.sukhetskii@gmail.com>
2024-03-30 20:45:55 +0200

    improvements

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,4 @@
-`use strict`;
+"use strict";
 
 function makeArray(firstArray, secondArray, maxLength) {
   const finalArray = firstArray.concat(secondArray);
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,13 @@
-`use strict`;
+"use strict";
 
 function filterArray(numbers, value) {
-  const array = [];
+  const filteredArray = [];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
-      array.push(numbers[i]);
+      filteredArray.push(numbers[i]);
     }
   }
-  return array;
+  return filteredArray;
 }
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
