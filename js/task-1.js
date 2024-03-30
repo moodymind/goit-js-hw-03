ace19be75238049cbb007029893cf40b7f340428
Austin Miller <vadim.sukhetskii@gmail.com>
2024-03-30 18:28:14 +0200

    .

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,13 @@
 `use strict`;
 
-function calculateTotal(number) {
-  let index = 0;
-  while (index <= number) {
-    number += 1;
-  }
-  return number;
+function slugify(title) {
+  const titleLowCase = title.toLowerCase();
+  const splitWords = titleLowCase.split(" ");
+  const slug = splitWords.join("-");
+  return slug;
 }
 
-console.log(calculateTotal(3));
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
